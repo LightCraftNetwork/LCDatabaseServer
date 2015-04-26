5df6f96f195d071752ec53a4e1f773281fdf253e
Justin <ArrayPro07@gmail.com>
2015-04-25 23:25:01 -0400

    [Unusable] Preview of new @api special category!

--- src/com/arrayprolc/lunadb/commands/CommandInsert.java
+++ src/com/arrayprolc/lunadb/commands/CommandInsert.java
@@ -2,6 +2,7 @@ package com.arrayprolc.lunadb.commands;
 
 import com.arrayprolc.lunadb.command.Command;
 import com.arrayprolc.lunadb.util.UtilFile;
+import com.arrayprolc.lunadb.util.UtilQuery;
 
 public class CommandInsert extends Command {
 
@@ -16,11 +17,43 @@ public class CommandInsert extends Command {
             return ("Usage: insert category key value (Please use _ in place of spaces.)");
         }
         try {
-            String category = args[0].toLowerCase();
-            String key = args[1];
+            final String category = args[0].toLowerCase();
+            final String key = args[1];
             String value = args[2];
-            UtilFile.save(category, key, value);
-            return ("SUCCESS: Inserting " + value + " into " + category + " with key " + key);
+            if (value.startsWith("@api?")) {
+                value = value.substring(5);
+                final String v = value;
+                Thread th = new Thread() {
+                    public void run() {
+                        String insert = "0";
+                        try {
+                            String url = new CommandRetrieve().runCommand("127.0.0.1", true, new String[] { "@api", v.split("!!")[0] }, true);
+                            for (int i = 0; i < 50; i++) {
+                                if (url.contains("$" + i) && v.contains("$" + i)) {
+                                    for (String s : v.split("!!")[1].split(",,")) {
+                                        String repl = s.split("=")[0];
+                                        String name = s.split("=")[1];
+                                        url = url.replace(repl, name);
+                                    }
+                                }
+                            }
+                            String s = UtilQuery.query(url);
+                            insert = s;
+
+                            UtilFile.save(category, key, insert);
+
+                        } catch (Exception ex) {
+                            UtilFile.save(category, key, "0");
+                            ex.printStackTrace();
+                        }
+                    }
+                };
+                th.start();
+                return "SUCCESS: Queueing insertion of API-found " + value + " into " + category + " with HTTP/API string " + key;
+            } else {
+                UtilFile.save(category, key, value);
+                return ("SUCCESS: Inserting " + value + " into " + category + " with key " + key);
+            }
         } catch (Exception ex) {
             return "FAILED: An error occured, please try again. || Usage: insert category key value (Please use _ in place of spaces.)";
         }
