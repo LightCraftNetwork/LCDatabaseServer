b2647a744772954869011181f04f21c7ae8517cc
Justin <ArrayPro07@gmail.com>
2015-05-10 14:51:36 -0400

    Changed insertscript to use Base64. Now requires Java 8, may look into Apache for users using 7.

--- src/com/arrayprolc/lunadb/commands/CommandInsertScript.java
+++ src/com/arrayprolc/lunadb/commands/CommandInsertScript.java
@@ -1,5 +1,7 @@
 package com.arrayprolc.lunadb.commands;
 
+import java.util.Base64;
+
 import com.arrayprolc.lunadb.command.Command;
 import com.arrayprolc.lunadb.util.UtilFile;
 
@@ -13,12 +15,12 @@ public class CommandInsertScript extends Command {
     @Override
     public String runCommand(String ip, boolean isLocal, String[] args, boolean isAdmin) {
         if (args.length == 0) {
-            return ("Usage: insert category key value (Please use _ in place of spaces.)");
+            return ("Usage: insert category key value (Please use Base64.)");
         }
         try {
             String category = args[0].toLowerCase();
             String key = args[1];
-            String value = args[2];
+            String value = Base64.getDecoder().decode(args[2]).toString();
             UtilFile.saveScript(category, key, value);
             return ("SUCCESS: Inserting " + value.length() + "-character-long script into " + category + " with key " + key);
         } catch (Exception ex) {
